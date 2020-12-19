168a95bbcdc72e1cd9e8af7830aa8a92e1a35045
DiamondUnturned <73734680+DiamondUnturned@users.noreply.github.com>
2020-12-19 11:56:21 +0300

    Add files via upload

---
+++ he/H.py
@@ -0,0 +1,44 @@
+
+
+print("У вас 3 попытки")
+name1 = input("Enter Password ")
+if name1 == "228909228":
+    print("Corrected Password")
+    exit(all)
+else:
+    print("Not Corrected Password")
+    print("У вас 2 попытки")
+
+
+name1 = input("Enter Password ")
+if name1 == "228909228":
+    print("Corrected Password")
+    
+else:
+    print("Not Corrected Password")
+    print("У вас 1 попытка")
+    
+name1 = input("Enter Password ")
+if name1 == "228909228":
+    print("Corrected Password")
+else:
+    print("Not Corrected Password")
+    print("Введите mp1 чтобы закрыть все")
+mp1 = input("Введите команду ")
+if mp1 == "mp1":
+    exit(all)
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
